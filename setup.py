b01fed097f0c9c01a64f03d931a40603d4eaaafa
Ryan Balfanz <rbalfanz@netflix.com>
2013-11-19 13:49:54 -0800

    Updated dist

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 from setuptools import setup, find_packages
 
 
@@ -5,7 +6,7 @@ setup(
 	name = "reservoir-sampling-cli",
 	version = "0.1",
 	description = "A command line tool to randomly sample k items from an input S containing n items.",
-	long_description=open('README.md').read(),
+	# long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
 	url = "https://github.com/RyanBalfanz/reservoir-sampling-cli",
 	license = 'MIT',
 	author = "Ryan Balfanz",
@@ -16,7 +17,14 @@ setup(
 			'resamp = sampler.command_line:main',
 		],
 	},
-	include_package_data = True,
-	exclude_package_data = { '': ['README.md'] },
+	# package_dir = {'':'.'},
+	# package_data = {
+	# 	# If any package contains *.txt or *.rst files, include them:
+	# 	'': ["README.md"],
+	# 	# And include any *.msg files found in the 'hello' package, too:
+	# 	# 'hello': ['*.msg'],
+	# },
+	# include_package_data = True,
+	# exclude_package_data = { '': ['words.txt'] },
 	test_suite = "tests"
 )
