09bc27d0ed308ca28b0a6d0bdc382bfc5bc322d1
Ryan Balfanz <rbalfanz@netflix.com>
2013-11-19 12:51:02 -0800

    Added addition metadata

--- setup.py
+++ setup.py
@@ -4,6 +4,12 @@ from setuptools import setup, find_packages
 setup(
 	name = "reservoir-sampling-cli",
 	version = "0.1",
+	description = "A command line tool to randomly sample k items from an input S containing n items.",
+	long_description=open('README.md').read(),
+	url = "https://github.com/RyanBalfanz/reservoir-sampling-cli",
+	license = 'MIT',
+	author = "Ryan Balfanz",
+	author_email = "ryan@ryanbalfanz.net",
 	packages = find_packages(),
 	entry_points = {
 		'console_scripts': [
